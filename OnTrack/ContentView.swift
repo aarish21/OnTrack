f87fa280f639e51f0a4db415bf5e11dcf28a7f05
aarish <aarish.jerry@gmail.com>
2022-05-30 01:30:20 +0530

    Initial Commit

---
+++ OnTrack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnTrack
+//
+//  Created by Aarish on 30/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnTrack/OnTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnTrackApp.swift
+//  OnTrack
+//
+//  Created by Aarish on 30/05/22.
+//
+
+import SwiftUI
+
+@main
+struct OnTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
